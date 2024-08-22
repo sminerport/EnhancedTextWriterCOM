7fb56f0ee5081d706d63bb34435c3f827df01d10
Scott <scott.miner.data.scientist@gmail.com>
2024-08-22 02:10:51 -0600

    update local .gitignore

---
+++ EnhancedTextWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace EnhancedTextWriterLib
+{
+   [ComVisible(true)]
+   [ClassInterface(ClassInterfaceType.None)]
+   [Guid("33FF6FC9-F461-4578-811D-04BB97BB9B31")]
+   public class EnhancedTextWriter : IEnhancedTextWriter
+   {
+      public void SaveToFile(string filePath, string text)
+      {
+         string directoryPath = Path.GetDirectoryName(filePath);
+         if (!Directory.Exists(directoryPath))
+         {
+            Directory.CreateDirectory(directoryPath);
+         }
+         File.WriteAllText(filePath, text);
+      }
+   }
+}
---
+++ IEnhancedTextWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace EnhancedTextWriterLib
+{
+   [ComVisible(true)]
+   [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+   [Guid("97FBE95E-5EDC-494C-A703-39E230DB9A6C")]
+   public interface IEnhancedTextWriter
+   {
+      void SaveToFile(string filePath, string text);
+   }
+}
